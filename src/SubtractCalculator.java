7414ca94982d3ee959473a39f647f537720f0c56
bmisra21 <42977074+bmisra21@users.noreply.github.com>
2018-09-17 19:45:02 -0400

    Add files via upload

---
+++ src/SubtractCalculator.java
@@ -0,0 +1,10 @@
+
+public class SubtractCalculator 
+{
+	public int sub (int num1, int num2) 
+	{
+		int ans = num1 - num2;
+		return ans;
+	}
+
+}
---
+++ src/SubtractCalculatorMain.java
@@ -0,0 +1,12 @@
+
+public class SubtractCalculatorMain {
+
+	public static void main(String[] args) 
+	{
+		SubtractCalculator calc = new SubtractCalculator();
+		System.out.print("The difference between 6 and 2 is: " + calc.sub(6, 2));
+		
+
+	}
+
+}
